ecd8813652ee9faa2d3c28cfd4de4280459d0b73
tomoya06 <peng4766@gmail.com>
2019-11-03 21:57:25 +0800

    dev:drawer

--- src/app/system/SystemBar/index.tsx
+++ src/app/system/SystemBar/index.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import {
   AppBar, Toolbar,
-  Button,
-  Box, Grid
+  Fab,
+  Button, IconButton,
+  Box, Grid,
+  Drawer, Dialog, Slide,
+  GridList, GridListTile, GridListTileBar, Container,
+  Typography,
 } from '@material-ui/core'
+
+import { TransitionProps } from '@material-ui/core/transitions';
+
+import {
+  DonutLarge as DonutLargeIcon,
+} from '@material-ui/icons';
 
 import SystemBarClock from '../../systembar/Clock';
 
@@ -21,13 +31,25 @@ type Props = {
   store: SystemStore,
 }
 
+
+const Transition = React.forwardRef<unknown, TransitionProps>(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 @observer
 export default class SystemBar extends React.Component<Props, State> {
   state: Readonly<State> = {
     isDrawerOpen: false,
   }
 
+  triggerDrawer = (flag: boolean) => {
+    this.setState({
+      isDrawerOpen: flag,
+    })
+  }
+
   render() {
+    const { isDrawerOpen } = this.state;
     const { store } = this.props;
     const { runningApps } = store;
 
@@ -37,24 +59,43 @@ export default class SystemBar extends React.Component<Props, State> {
           <Toolbar variant="dense">
             <Grid container>
               <Box>
-                <Button>START</Button>
+                <IconButton onClick={() => this.triggerDrawer(true)}>
+                  <DonutLargeIcon />
+                </IconButton>
               </Box>
-              <Box flexGrow={1}>
+              <Box flexGrow={1} alignItems="center" display="flex">
                 {runningApps.map((app) => (
                   <Button key={`systembar-running_app-${app.pid}`}>{app.displayName}</Button>
                 ))}
               </Box>
-              <Box>
+              <Box alignItems="center" display="flex">
                 <SystemBarClock />
               </Box>
             </Grid>
           </Toolbar>
         </AppBar>
-        {
-          this.state.isDrawerOpen && (
-            <div className="app-drawer"></div>
-          )
-        }
+        <Dialog
+          fullScreen open={isDrawerOpen}
+          TransitionComponent={Transition}
+          onClose={() => this.triggerDrawer(false)}
+        >
+          <AppBar position="relative">
+            <Toolbar variant="dense">
+              <Fab onClick={() => this.triggerDrawer(false)} className="drawer-fab">
+                <DonutLargeIcon />
+              </Fab>
+            </Toolbar>
+          </AppBar>
+          <Container maxWidth="md">
+            <GridList cellHeight={160} cols={3}>
+              <GridListTile cols={1} rows={1}>
+                <Box height="100%" bgcolor="text.disabled">
+                  <Typography>TEST</Typography>
+                </Box>
+              </GridListTile>
+            </GridList>
+          </Container>
+        </Dialog>
       </div>
     )
   }
